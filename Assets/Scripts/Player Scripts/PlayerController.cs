ca20dbe55e0918c142f4d052dbb3305e1fafab3b
Joakim Pedersen Nilfjord <joakim.nilfjord@gmail.com>
2018-04-25 15:34:42 +0700

    camera rotation

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -7,14 +7,11 @@ namespace Player_Scripts
 	
 	public class PlayerController : MonoBehaviour
 	{
-		[SerializeField]
-		private float _speed = 5f;
-		
-		[SerializeField]
-		private float _lookSensitivity = 5f;
-		
+		[SerializeField] private float _speed = 5f;
+		[SerializeField] private float _lookSensitivity = 5f;
 		private PlayerMotor _motor;
-		
+
+	
 		private void Start() {
 			_motor = GetComponent<PlayerMotor> ();
 		}
@@ -23,31 +20,46 @@ namespace Player_Scripts
 		private void Update()
 		{
 			MovementInput();
+			CameraRotationInput();
+			CameraRotation();
 			
 		}
+		
+		
 		/*
-		 * Input to enable the player walk (W,S,A,D).
+		 * Applies Input to enable the player walk (W,S,A,D).
 		 * The input is sent to Player Motor.
 		 */
 		private void MovementInput()
 		{
 			var xMove = Input.GetAxis("Horizontal");
 			var yMove = Input.GetAxis("Vertical");
-			var moveHorizontal = transform.right * xMove; 
+			var moveHorizontal = transform.right * xMove;  
 			var moveVertical = transform.forward * yMove; 
-			var velocity = (moveHorizontal + moveVertical).normalized * _speed;
+			
+			var velocity = (moveHorizontal + moveVertical).normalized* _speed; 
 			_motor.Move(velocity);
 		}
 		/*
-		 * Input to enable player to turn around.
+		 * Applies Input to enable player to turn around.
 		 * We are turning around the Y axis based on the mouse positiion of X.
 		 */
 		private void CameraRotationInput()
 		{
 			var rotationAroundY = Input.GetAxis ("Mouse X");
 			var rotation = new Vector3 (0f, rotationAroundY, 0f) * _lookSensitivity;
+			_motor.Rotate(rotation);
 			
-			
+		}
+		/*
+		 * Applies Input to tilt camera up and down.
+		 */
+		private void CameraRotation()
+		{
+			var cameraRotationAroundX = Input.GetAxis ("Mouse Y");
+			var rotation = new Vector3 (cameraRotationAroundX, 0f, 0f) * _lookSensitivity;
+			_motor.RotateCamera(rotation);
+
 		}
 
 
--- Assets/Scripts/Player Scripts/PlayerMotor.cs
+++ Assets/Scripts/Player Scripts/PlayerMotor.cs
@@ -1,24 +1,30 @@
-﻿using UnityEngine;
+﻿using System.CodeDom;
+using UnityEngine;
 
 namespace Player_Scripts
 {
 	[RequireComponent(typeof(Rigidbody))]
 	
-	public class PlayerMotor : MonoBehaviour {
+	public class PlayerMotor : MonoBehaviour
+	{
+		[SerializeField] private Camera _camera;
 		private Rigidbody _rigidbody;
 		private Vector3 _velocity = Vector3.zero;
-		
 		private Vector3 _rotation = Vector3.zero;
-
+		private Vector3 _cameraRotation = Vector3.zero;
+		
 
 		private void Start()
 		{
 			_rigidbody = GetComponent<Rigidbody>();
+			
 		}
 
 		private void FixedUpdate()
 		{
 			PerformMovement();
+			PerformRotation();
+			PerformCameraRotation();
 			
 		}
 
@@ -29,6 +35,7 @@ namespace Player_Scripts
 		public void Move(Vector3 newVelocity)
 		{
 			_velocity = newVelocity;
+			
 		}
 		
 		/*
@@ -40,9 +47,15 @@ namespace Player_Scripts
 			_rotation = newRotation;
 		}
 
+
+		public void RotateCamera(Vector3 newCameraRotation)
+		{
+			_cameraRotation = newCameraRotation;
+		}
+
 		/*
-		 * PerformsMovement on rigidbody
-		 * if it has velocity
+		 * Performs movement on rigidbody, makes player walker
+		 * if it has velocity. i.e keyboard is pressed (w,A,S,D)
 		 */
 		private void PerformMovement()
 		{
@@ -51,20 +64,27 @@ namespace Player_Scripts
 				_rigidbody.MovePosition(_rigidbody.position + _velocity * Time.deltaTime) ;
 			}
 		}
-
+		/*
+		 * Performs rotation on rigidbody
+		 * if it has a new rotation, i.e the mouse moved 
+		 */
 		private void PerformRotation()
 		{
 			if (_rotation != Vector3.zero)
-			{	//rotation is a Quatarnion
+			{	//rotation is a Quatarnion, _rotation is a vector3 and turned into Quatarnion with Euler function
 				_rigidbody.MoveRotation(_rigidbody.rotation * Quaternion.Euler(_rotation));
 			}
 		}
 
+		private void PerformCameraRotation()
+		{
+			if (_camera != null)
+			{
+				_camera.transform.Rotate(-_cameraRotation);
+			}
 
-
-
-
-
+			
+		}
 
 	}
 }
