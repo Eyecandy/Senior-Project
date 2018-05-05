4b45f1687737d7939c524463c6938c2c03fff20f
Dev Singh <dev.thep@hotmail.com>
2018-05-05 16:42:43 +0700

    Integrated Ball Physics with current build

---
+++ Assets/Scripts/Ball Scripts/BallManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class BallManager : NetworkBehaviour
+{
+
+	[SerializeField] private Transform plane;
+	[SerializeField] private GameObject sphere;
+	[SerializeField] private int numberOfBalls;
+	private float cornerX;
+	private float cornerY;
+	private float cornerZ;
+	private Vector3 corner;
+	private float span;
+	public Vector3[] positions; 
+
+	// Use this for initialization
+	void Start () {
+		cornerX = (plane.localScale.x / 2)*10 + plane.position.x;
+		cornerY = plane.position.y;
+		cornerZ = (plane.localScale.z / 2)*10 + plane.position.z - 1;
+		corner = new Vector3(cornerX,cornerY,cornerZ);
+		span = ((plane.transform.localScale.x * 10f)/numberOfBalls) - 1.5f;
+		Vector3 spanVector3 = new Vector3(span, 0f, 0f);
+		positions = new Vector3[numberOfBalls];
+		for (int i = 0; i < numberOfBalls; i++)
+		{
+			positions[i] = corner - spanVector3;
+			Debug.Log(positions[i]);
+			var obj = Instantiate(sphere, positions[i], Quaternion.identity);
+			obj.name = "Ball-"+i.ToString();
+			corner = corner - spanVector3;
+		}
+		
+	}
+}
---
+++ Assets/Scripts/Ball Scripts/BallMotor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BallMotor : NetworkBehaviour
+{
+
+	private Rigidbody rb;
+	[SerializeField] private float thrust;
+	private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		GameObject spawner = GameObject.FindGameObjectWithTag("Spawner");
+		BallManager manager = spawner.GetComponent<BallManager>();
+		int ballNumber = Int32.Parse(this.name.Split('-')[1]);
+		startPosition = manager.positions[ballNumber];
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (this.transform.position.y < -1)
+		{
+			this.transform.position = startPosition;
+			rb.velocity = Vector3.zero;
+		}
+		rb.AddForce(new Vector3(0,0,-1) * thrust);
+	}
+}
