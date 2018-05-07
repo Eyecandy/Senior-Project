864cbbe7f388bee0ab3f3675b436fe6f852f173b
Dev Singh <dev.thep@hotmail.com>
2018-05-07 15:14:34 +0700

    Ball Spawner & Ball Motor Fix

--- Assets/Scripts/Ball Scripts/BallMotor.cs
+++ Assets/Scripts/Ball Scripts/BallMotor.cs
@@ -1,31 +1,29 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Networking;
 
-public class BallMotor : NetworkBehaviour
+namespace Ball_Scripts
 {
-
-	private Rigidbody rb;
-	[SerializeField] private float thrust;
-	private Vector3 startPosition;
-
-	// Use this for initialization
-	void Start ()
+	public class BallMotor : NetworkBehaviour
 	{
-		rb = GetComponent<Rigidbody>();
-		GameObject spawner = GameObject.FindGameObjectWithTag("Spawner");
-		BallSpawner manager = spawner.GetComponent<BallSpawner>();
-		int ballNumber = Int32.Parse(this.name.Split('-')[1]);
-		startPosition = manager.positions[ballNumber];
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		if (this.transform.position.y < -1)
+
+		private Rigidbody _rb;
+		[SerializeField] private float _thrust;
+		public Vector3 StartPosition;
+
+		// Use this for initialization
+		void Start ()
 		{
-			this.transform.position = startPosition;
-			rb.velocity = Vector3.zero;
+			_rb = GetComponent<Rigidbody>();
 		}
-		rb.AddForce(new Vector3(0,0,-1) * thrust);
+	
+		// Update is called once per frame
+		void FixedUpdate () {
+			if (this.transform.position.y < -1)
+			{
+				this.transform.position = StartPosition;
+				_rb.velocity = Vector3.zero;
+			}
+			_rb.AddForce(new Vector3(0,0,-1) * _thrust);
+		}
 	}
 }
--- Assets/Scripts/Ball Scripts/BallSpawner.cs
+++ Assets/Scripts/Ball Scripts/BallSpawner.cs
@@ -1,36 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Networking;
 
-public class BallSpawner : NetworkBehaviour
+namespace Ball_Scripts
 {
-    [SerializeField] private GameObject ballPrefab;
-    [SerializeField] private int numberOfBalls;
-    [SerializeField] private Transform plane;
-    private float cornerX;
-    private float cornerY;
-    private float cornerZ;
-    private Vector3 corner;
-    private float span;
-    public Vector3[] positions; 
-
-    public override void OnStartServer()
+    public class BallSpawner : NetworkBehaviour
     {
-        cornerX = (plane.localScale.x / 2)*10 + plane.position.x;
-        cornerY = plane.position.y+1.78f;
-        cornerZ = (plane.localScale.z / 2)*10 + plane.position.z - 1;
-        corner = new Vector3(cornerX,cornerY,cornerZ);
-        span = ((plane.transform.localScale.x * 10f)/numberOfBalls) - 1.5f;
-        Vector3 spanVector3 = new Vector3(span, 0f, 0f);
-        positions = new Vector3[numberOfBalls];
-        for (int i = 0; i < numberOfBalls; i++)
+        [SerializeField] private GameObject _ballPrefab;
+        [SerializeField] private int _numberOfBalls;
+        [SerializeField] private Transform _plane;
+        private float _cornerX;
+        private float _cornerY;
+        private float _cornerZ;
+        private Vector3 _corner;
+        private float _span;
+        public Vector3[] Positions; 
+
+        public override void OnStartServer()
         {
-            positions[i] = corner - spanVector3;
-            GameObject ball = Instantiate(ballPrefab, positions[i], Quaternion.identity);
-            ball.name = "Ball-"+i.ToString();
-            corner = corner - spanVector3;
-            NetworkServer.Spawn(ball);
+            _cornerX = (_plane.localScale.x / 2)*10 + _plane.position.x;
+            _cornerY = _plane.position.y+1.78f;
+            _cornerZ = (_plane.localScale.z / 2)*10 + _plane.position.z - 1;
+            _corner = new Vector3(_cornerX,_cornerY,_cornerZ);
+            _span = ((_plane.transform.localScale.x * 10f)/_numberOfBalls) - 1.5f;
+            Vector3 spanVector3 = new Vector3(_span, 0f, 0f);
+            Positions = new Vector3[_numberOfBalls];
+            for (int i = 0; i < _numberOfBalls; i++)
+            {
+                Positions[i] = _corner - spanVector3;
+                GameObject ball = Instantiate(_ballPrefab, Positions[i], Quaternion.identity);
+                BallMotor ballMotor = ball.GetComponent<BallMotor>();
+                ballMotor.StartPosition = Positions[i];
+                ball.name = "Ball-"+i.ToString();
+                _corner = _corner - spanVector3;
+                NetworkServer.Spawn(ball);
+            }
         }
     }
 }
