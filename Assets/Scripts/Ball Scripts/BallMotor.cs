81500af445a26171c3793ff71f288fdf765825c0
Dev Singh <dev.thep@hotmail.com>
2018-05-12 17:04:18 +0700

    Teleport without interpolation improved

--- Assets/Scripts/Ball Scripts/BallMotor.cs
+++ Assets/Scripts/Ball Scripts/BallMotor.cs
@@ -1,14 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.Networking;
-using Smooth;
+//using Smooth;
 
 namespace Ball_Scripts
 {
 	public class BallMotor : NetworkBehaviour
 	{
-		SmoothSync _smoothSync;
+//		SmoothSync _smoothSync;
 
 		private Rigidbody _rb;
+		private MeshRenderer _renderer;
+		private SphereCollider _collider;
 		[SerializeField] private float _thrust;
 		[SyncVar] public string BallName;
 		[SyncVar] public Vector3 StartPosition;
@@ -17,7 +20,8 @@ namespace Ball_Scripts
 		void Start ()
 		{
 			_rb = GetComponent<Rigidbody>();
-			_smoothSync = GetComponent<SmoothSync>();
+//			_smoothSync = GetComponent<SmoothSync>();
+			_renderer = GetComponent<MeshRenderer>();
 			this.name = BallName;
 
 		}
@@ -26,20 +30,30 @@ namespace Ball_Scripts
 		void FixedUpdate () {
 			if (this.transform.position.y < -1)
 			{
-				this.transform.position = StartPosition;
+				if (GetComponent<NetworkIdentity>().isServer && GetComponent<NetworkIdentity>().isClient)
+				{
+					_renderer.enabled = false;
+					StartCoroutine(Respawn());
+				}
 //				Debug.Log("Respawn Ball " + StartPosition + " " + transform.rotation);
-				//Network timestamp
-				int timestamp = NetworkTransport.GetNetworkTimestamp();
-				// Teleport owned object
-//				_smoothSync.teleport(timestamp, StartPosition, transform.rotation);
-				_rb.velocity = Vector3.zero;
 			}
 			_rb.AddForce(new Vector3(0,0,-1) * _thrust);
+		}
+		
+		private IEnumerator Respawn()
+		{
+			yield return new WaitForSeconds(1.25f);
+			Debug.Log("Respawned");
+			_rb.position = StartPosition;
+			_rb.velocity = Vector3.zero;
+			_renderer.enabled = true;
 		}
 
 		public void SetBallName(string ballName)
 		{
 			BallName = ballName;
 		}
+		
+		
 	}
 }
