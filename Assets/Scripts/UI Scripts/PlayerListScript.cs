6c08832889a918aefdba5affb56226d397102a0f
Joakim Pedersen Nilfjord <joakim.nilfjord@gmail.com>
2018-06-02 00:25:00 +0700

    Score board
    Panel for deaths of player

---
+++ Assets/Scripts/UI Scripts/PlayerListScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerListScript : MonoBehaviour
+{
+
+	[SerializeField] private GameObject _individualPlayerEntryPrefab;
+	
+	
+	
+	public void ListOutScores()
+	{
+		Debug.Log("LIST OUT SCORES CALLED");
+		foreach (var player in GameManager.GetAllPlayers())
+		{
+			
+			var playerVal = player.Value;
+			var playerScoreInstance = Instantiate(_individualPlayerEntryPrefab, transform);
+			playerScoreInstance.transform.SetParent(transform);
+			var individualPlayerScoreScript = playerScoreInstance.GetComponent<PlayerEntry>();
+			individualPlayerScoreScript.Setup(playerVal.name,playerVal.NumberOfDeaths);
+			
+					
+		}
+
+	}
+
+	public void ClearOutScores()
+	{
+		foreach (Transform child in this.transform )
+		{
+			Destroy(child.gameObject);
+		}
+	}
+
+
+
+
+}
---
+++ Assets/Scripts/UI Scripts/Score.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+
+	[SerializeField] private GameObject _playerListGameObject;
+	
+	
+
+	
+	public void OnEnable()
+	{
+		Debug.Log("Enabled SCORE PANEL");
+		_playerListGameObject.GetComponent<PlayerListScript>().ListOutScores();
+		
+		
+
+	}
+
+	private void OnDisable()
+	{
+		_playerListGameObject.GetComponent<PlayerListScript>().ClearOutScores();
+	}
+
+	
+
+}
