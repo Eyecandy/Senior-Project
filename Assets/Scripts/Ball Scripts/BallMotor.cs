ea1a0387713fa5d5b431cc361032945fb1044d59
Dev Singh <dev.thep@hotmail.com>
2018-06-01 17:58:55 +0700

    Ball spawn variation & Server controlled ball motor

--- Assets/Scripts/Ball Scripts/BallMotor.cs
+++ Assets/Scripts/Ball Scripts/BallMotor.cs
@@ -8,16 +8,28 @@ namespace Ball_Scripts
 {
 	public class BallMotor : NetworkBehaviour
 	{
-		
-
 		public Rigidbody _rb;
 		private MeshRenderer _renderer;
 		private SphereCollider _collider;
 		[SerializeField] private float _thrust;
+		[SerializeField] private Transform _plane;
 		[SyncVar] public string BallName;
 		[SyncVar] public Vector3 StartPosition;
+//		private Vector3 _directionalPush;
+		[SyncVar] private int _currentSide = 0;
+		private bool _isFalling = false;
 
-		private Vector3 _directionalPush;
+		private Vector3[] _sideForce = new[] {
+			new Vector3(0, 0, -1), //Side 0
+			new Vector3(1, 0, 0),  //Side 1
+			new Vector3(-1, 0, 0), //Side 2
+		};
+		private float _planeSide;
+		
+		public void SetBallName(string ballName)
+		{
+			BallName = ballName;
+		}
 
 		// Use this for initialization
 		void Start()
@@ -26,54 +38,57 @@ namespace Ball_Scripts
 			_renderer = GetComponent<MeshRenderer>();
 			this.name = BallName;
 			GameManager.RegisterBallMotor(BallName,this);
-//			if (!GetComponent<NetworkIdentity>().isServer)
-//			{
-//				this.enabled = false;
-//			}
+			_planeSide = (_plane.localScale.x / 2)*10;
 		}
 
 
 		// Update is called once per frame
+		[ServerCallback]
 		void FixedUpdate()
 		{
-			if (GetComponent<NetworkIdentity>().isServer)
+			if (!_isFalling)
 			{
-				CmdAddForce();
-				if (this.transform.position.y < -1)
-				{
-					_renderer.enabled = false;
-					CmdRespawn();
-//				Debug.Log("Respawn Ball " + StartPosition + " " + transform.rotation);
-				}
+				_rb.AddForce(_sideForce[_currentSide] * _thrust);
+			}
+			if (transform.position.y < -1 && !_isFalling)
+			{
+				_isFalling = true;
+				Debug.Log("Server Ball Respawn");
+				_renderer.enabled = false;
+				StartPosition = RandomizeStart();
+				StartCoroutine(Respawn());
+//				CmdRespawn();
 			}
 		}
 
-		[Command]
-		void CmdRespawn()
-		{
-			RpcRespawnOnAllClients();
-		}
+//		[Command]
+//		void CmdRespawn()
+//		{
+//			Debug.Log("CMD Respawn");
+//			RpcRespawnOnAllClients();
+//		}
+//
+//		[ClientRpc]
+//		void RpcRespawnOnAllClients()
+//		{
+//			Debug.Log("RPC Respawn");
+//			StartCoroutine(Respawn());
+//		}
 
-		[ClientRpc]
-		void RpcRespawnOnAllClients()
-		{
-			StartCoroutine(Respawn());
-		}
+//		[Command]
+//		void CmdAddForce()
+//		{
+//			RpcAddForce();
+//		}
 
-		[Command]
-		void CmdAddForce()
-		{
-			RpcAddForce();
-		}
-
-		[ClientRpc]
-		void RpcAddForce()
-		{
-			if (_rb != null)
-			{
-				_rb.AddForce(new Vector3(0, 0, -1) * _thrust);
-			}
-		}
+//		[ClientRpc]
+//		void RpcAddForce()
+//		{
+//			if (_rb != null)
+//			{
+//				_rb.AddForce(_sideForce[_currentSide] * _thrust);
+//			}
+//		}
 		
 		/*
 
@@ -96,24 +111,34 @@ namespace Ball_Scripts
 			_rb.AddForce( _directionalPush* force * isPush ,ForceMode.VelocityChange);
 		}
 		*/
-		
-		
 
 		private IEnumerator Respawn()
 		{
-			yield return new WaitForSeconds(1.25f);
+			// TO DO: Randomize Respawn Time
+			yield return new WaitForSeconds(1f);
 			_rb.position = StartPosition;
+			_rb.rotation = Quaternion.identity;
 			_rb.velocity = Vector3.zero;
+			yield return new WaitForSeconds(0.45f);
 			_renderer.enabled = true;
+			_isFalling = false;
 		}
 
-		public void SetBallName(string ballName)
+		private Vector3 RandomizeStart()
 		{
-			BallName = ballName;
-			
-			
+			var side = Random.Range(0, 3);
+			var location = Random.Range(-_planeSide, _planeSide);
+			_currentSide = side;
+			if (side == 0){
+				Debug.Log("SIDE " + side);
+				return new Vector3(location,0f,_planeSide-2.2f);
+			} else if (side == 1){
+				Debug.Log("SIDE " + side);
+				return new Vector3(-_planeSide+1.78f,0f,location);
+			} else{
+				Debug.Log("SIDE " + side);
+				return new Vector3(_planeSide-1.78f,0f,location);
+			}
 		}
-		
-		
 	}
 }
