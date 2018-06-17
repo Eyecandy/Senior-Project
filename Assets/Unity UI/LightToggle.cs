a2bc7f66f76d890ca7adcabb275a639b8c2672c2
Dev Singh <dev.thep@hotmail.com>
2018-06-17 22:01:47 +0700

    Main Menu

---
+++ Assets/Unity UI/LightToggle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SpriteGlow;
+using UnityEngine;
+
+public class LightToggle : MonoBehaviour
+{
+	private SpriteGlowEffect _brakeLight;
+	private bool _descend;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_brakeLight = GetComponent<SpriteGlowEffect>();
+		_brakeLight.GlowBrightness = 1.54f;
+		_descend = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (_descend)
+		{
+			_brakeLight.GlowBrightness -= 0.01f;
+			if (_brakeLight.GlowBrightness <= 1.54f)
+			{
+				_descend = false;
+			}
+		}
+		else
+		{
+			_brakeLight.GlowBrightness += 0.01f;
+			if (_brakeLight.GlowBrightness >= 2.1f)
+			{
+				_descend = true;
+			}
+		}
+		
+	}
+}
---
+++ Assets/Unity UI/LoadSceneOnClick.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Unity_UI
+{
+	public class LoadSceneOnClick : MonoBehaviour {
+
+		public void LoadByIndex(int sceneIndex)
+		{
+			SceneManager.LoadScene(sceneIndex);
+		}
+	}
+}
