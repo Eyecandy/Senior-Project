f4c1da433e33983366f4687a5a0ff19fd8aedd11
Dev Singh <dev.thep@hotmail.com>
2018-06-18 21:57:36 +0700

    Added background spotlight

--- Assets/Unity UI/LightToggle.cs
+++ Assets/Unity UI/LightToggle.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using SpriteGlow;
+﻿using SpriteGlow;
 using UnityEngine;
 
 public class LightToggle : MonoBehaviour
---
+++ Assets/Unity UI/SpotlightColor.cs
@@ -0,0 +1,39 @@
+﻿using SpriteGlow;
+using UnityEngine;
+using System;
+
+namespace Unity_UI
+{
+	public class SpotlightColor : MonoBehaviour {
+
+		private SpriteGlowEffect _spotLight;
+		private float _timeBeforeSwitch = 3f;
+		private Color[] _colors = new Color[4];
+		private int _current;
+
+		// Use this for initialization
+		void Start ()
+		{
+			_spotLight = GetComponent<SpriteGlowEffect>();
+			_spotLight.GlowColor = Color.cyan;
+			_current = 0;
+			_colors[0] = Color.cyan;
+			_colors[1] = Color.red;
+			_colors[2] = Color.yellow;
+			_colors[3] = Color.green;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+			_timeBeforeSwitch -= Time.deltaTime;
+			if (_timeBeforeSwitch <= 0f)
+			{
+				_spotLight.GlowColor = _colors[_current];
+				_timeBeforeSwitch = 3f;
+				_current = (_current + 1) % _colors.Length;
+			}
+			
+		}
+	}
+}
