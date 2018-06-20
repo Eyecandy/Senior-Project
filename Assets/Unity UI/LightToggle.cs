494b5d16103464f0ff5b2fd8a734601be6b322cc
Dev Singh <dev.thep@hotmail.com>
2018-06-20 18:18:37 +0700

    Lobby Overhaul

--- Assets/Unity UI/LightToggle.cs
+++ Assets/Unity UI/LightToggle.cs
@@ -1,38 +1,41 @@
 ﻿using SpriteGlow;
 using UnityEngine;
 
-public class LightToggle : MonoBehaviour
+namespace Unity_UI
 {
-	private SpriteGlowEffect _brakeLight;
-	private bool _descend;
+	public class LightToggle : MonoBehaviour
+	{
+		private SpriteGlowEffect _brakeLight;
+		private bool _descend;
 
-	// Use this for initialization
-	void Start ()
-	{
-		_brakeLight = GetComponent<SpriteGlowEffect>();
-		_brakeLight.GlowBrightness = 1.54f;
-		_descend = false;
-	}
+		// Use this for initialization
+		void Start ()
+		{
+			_brakeLight = GetComponent<SpriteGlowEffect>();
+			_brakeLight.GlowBrightness = 1.54f;
+			_descend = false;
+		}
 	
-	// Update is called once per frame
-	void Update ()
-	{
-		if (_descend)
+		// Update is called once per frame
+		void Update ()
 		{
-			_brakeLight.GlowBrightness -= 0.01f;
-			if (_brakeLight.GlowBrightness <= 1.54f)
+			if (_descend)
 			{
-				_descend = false;
+				_brakeLight.GlowBrightness -= 0.01f;
+				if (_brakeLight.GlowBrightness <= 1.54f)
+				{
+					_descend = false;
+				}
 			}
-		}
-		else
-		{
-			_brakeLight.GlowBrightness += 0.05f;
-			if (_brakeLight.GlowBrightness >= 2.5f)
+			else
 			{
-				_descend = true;
+				_brakeLight.GlowBrightness += 0.05f;
+				if (_brakeLight.GlowBrightness >= 2.5f)
+				{
+					_descend = true;
+				}
 			}
-		}
 		
+		}
 	}
 }
